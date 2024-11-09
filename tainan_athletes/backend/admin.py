18c3575ad382a62434ae7e2794a06ddb630ad286
Frenzied_Dog <hck134599@gmail.com>
2024-11-09 14:40:22 +0800

    update .gitignore

--- tainan_athletes/backend/admin.py
+++ tainan_athletes/backend/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Athlete, Coach, HurtRecord, RaceRecord, DailyTrainRecord
 
 # Register your models here.
+admin.site.register(Athlete)
+admin.site.register(Coach)
+admin.site.register(HurtRecord)
+admin.site.register(RaceRecord)
+admin.site.register(DailyTrainRecord)
